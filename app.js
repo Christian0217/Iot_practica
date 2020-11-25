5ed00d71cde7e013c4c6d10c9c8a851f06fa5189
Christian0217 <74904014+Christian0217@users.noreply.github.com>
2020-11-25 19:30:35 +0100

    Firebase llegint correctament
    Afegides les línees que llegeixen les dades de la Firebase. El botó de llegir dades va mostrant les dades noves que apareixen.

--- app.js
+++ app.js
@@ -1,51 +1,35 @@
-Vue.component('secure-button', {
-  data: function(){
+Vue.component('fancy-chronometer', {
+  data: function() {
     return{
-      enabled: false,
-    }
-  }, //Quan executes $emit, abaix ho captura (en el v-on?)
-  template: `<div style="display:inline-block;padding:10px;border:solid">
-  <button v-on:click="$emit('click')" v-bind:disabled="! enabled"> Reset </button>
-  <label><input type="checkbox" v-model="enabled"> Enable </label>
-  </div>`
-});
-
-
-Vue.component('fancy-chronometer', { //genero un tag especial que podré utilitzar on sigui! Al segon paràmetre poso les opcions
-  data: function() { //S'ha de crear com una funció que retorna un objecte
-    return{
-        time: 0,
-        enabled: false,
+        dades: [],
     }
   },
-  props: ['chronoLabel'], //coloquem les propietats que usarem. Fixa't que la propietat label està abaix posada al html (template). Les propietats (variables) seran reactives. Fixa't que label, només la mostrem al primer requadre posant <span v-if="label">. Si aixo no estigués, els dos punts (:) estarien als 3 quadrats del html
   methods: {
     clickButton: function(){
-      this.enabled = false; //Fem que quan piqui a RESET, el enabled es desmarqui i que per tant el botó es desactivi
-      this.time=0
+
+      var starCountRef = firebase.database().ref('/').once('value', (snapshot) => {
+        var dadesNoves = []
+        for (valor in snapshot.val()){ //ens retorna la BD tota de una en un JSON i el recorrem
+            dadesNoves.push(snapshot.val()[valor])
+        }
+        if(this.dades !== dadesNoves){
+          this.dades = dadesNoves;
+        }
+      });
+
     }
   },
   template: `<div style="display:inline-block;padding:10px;border:solid">
-  <span v-if="chronoLabel"> {{chronoLabel}}: </span>
-  {{time}}
-  <button v-on:click="clickButton()" v-bind:disabled="! enabled"> Reset </button>
-  <label><input type="checkbox" v-model="enabled"> Enable </label>
+  <ul>
+    <li v-for="item in dades"> {{item}} </li>
+  </ul>
+  <button v-on:click="clickButton()"> Mostrar dades </button>
   </div>`,
-  created: function(){
-    setInterval(() => this.time++, 1000); //Podrem utilitzar el this per agafar el time de "data". La funció setInterval és pròpia de Vuejs
-  }
 });
-
 
 var vm = new Vue({
   el: '#app',
-  data: {
-    label2: "Hola" //Lliguem una instància de Vue amb una prop del objecte (la prop de fancy-chronometer)! Amazing
-  },
   template: `<div>
-  <fancy-chronometer chrono-label="Crono 1"></fancy-chronometer>
-  <fancy-chronometer v-bind:chrono-label="label2"></fancy-chronometer>
   <fancy-chronometer></fancy-chronometer>
-  <secure-button></secure-button>
   </div>`,
-}); 
+});
---
+++ config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAEbpFuAky2Ij9SuRNAcFgxVcwIcdv2eq8",
+  authDomain: "practica-iot-christian.firebaseapp.com",
+  databaseURL: "https://practica-iot-christian.firebaseio.com",
+  projectId: "practica-iot-christian",
+  storageBucket: "practica-iot-christian.appspot.com",
+  messagingSenderId: "1084457466479",
+  appId: "1:1084457466479:web:ed797dde55abc0149a6b9a"
+};
+
+firebase.initializeApp(firebaseConfig);
